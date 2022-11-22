174a6727402ecbffa2598dc7864a6be6e2e9cbb3
Leyuan Qian <lyqian@umich.edu>
2022-11-22 16:54:47 -0500

    updated unit test cases

--- tests/testthat/test-slr_beta.R
+++ tests/testthat/test-slr_beta.R
@@ -1,4 +1,5 @@
 test_that("slr_beta works", {
+  ## element-by-element test using iris data
   data(iris)
   standard = summary(lm(Sepal.Length ~ Sepal.Width, data = iris))$coefficients
   expect_equal(slr_beta(iris$Sepal.Width, iris$Sepal.Length)[1, 1], standard[1, 1])
@@ -9,4 +10,5 @@ test_that("slr_beta works", {
   expect_equal(slr_beta(iris$Sepal.Width, iris$Sepal.Length)[2, 3], standard[2, 3])
   expect_equal(slr_beta(iris$Sepal.Width, iris$Sepal.Length)[1, 4], standard[1, 4])
   expect_equal(slr_beta(iris$Sepal.Width, iris$Sepal.Length)[2, 4], standard[2, 4])
+
 })
--- tests/testthat/test-slr_rsquared.R
+++ tests/testthat/test-slr_rsquared.R
@@ -1,10 +1,15 @@
 test_that("multiplication works", {
+  ## test using iris data, modeling sepal widths (predictor) with sepal lengths (response).
   data(iris)
   standard1=summary(lm(Sepal.Length~Sepal.Width,data=iris))$r.squared
   expect_equal(slr_rsquared(iris$Sepal.Width,iris$Sepal.Length),standard1)
+
+  ## test using mtcars data, modeling gross horsepower (predictor) with 1/4 mile time (response).
   data(mtcars)
   standard2=summary(lm(qsec~hp,data=mtcars))$r.squared
   expect_equal(slr_rsquared(mtcars$hp,mtcars$qsec),standard2)
+
+  ## test using trees data, modeling tree diameter (girth) with tree height (response).
   data(trees)
   standard3=summary(lm(Height~Girth,data=trees))$r.squared
   expect_equal(slr_rsquared(trees$Girth,trees$Height),standard3)
